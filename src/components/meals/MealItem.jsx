6154d6c5c578c3bd61a38eda253bf81a8ff75388
Madhuri S <madhusabbineni@gmail.com>
2022-03-24 13:33:05 -0500

    Added mealItemForm for each mealItem & ui/input

--- src/components/meals/MealItem.jsx
+++ src/components/meals/MealItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const { meal } = props;
   const price = `$${meal.price.toFixed(2)}`;
   return (
-    <li>
-      <div className={classes.meal}>
+    <li className={classes.meal}>
+      <div>
         <h3>{meal.name}</h3>
         <div className={classes.description}>{meal.description}</div>
         <div className={classes.price}>{price} </div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
