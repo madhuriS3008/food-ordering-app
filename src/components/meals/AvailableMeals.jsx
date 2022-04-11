18b6eadc1503c81191f0990b224c570d8871cbe5
Madhuri S <madhusabbineni@gmail.com>
2022-04-11 15:59:48 -0500

    added custom useHttp hook for data fetching

--- src/components/meals/AvailableMeals.jsx
+++ src/components/meals/AvailableMeals.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { DUMMY_MEALS } from "../meals";
 import Card from "../ui/Card";
 import MealItem from "./MealItem";
+import useHttp from "../../hooks/useHttp";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+
+  const transformMeals = useCallback((mealsObj) => {
+    const loadedMeals = [];
+    for (const mealKey in mealsObj) {
+      loadedMeals.push({
+        id: mealKey,
+        name: mealsObj[mealKey].name,
+        description: mealsObj[mealKey].description,
+        price: mealsObj[mealKey].price,
+      });
+    }
+    setMeals(loadedMeals);
+  }, []);
+
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp(transformMeals);
+
+  useEffect(() => {
+    fetchMeals({
+      url: "https://food-ordering-app-26828-default-rtdb.firebaseio.com/meals.json",
+    });
+  }, [fetchMeals]);
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
+
+  let content = mealsList;
+  if (error) {
+    content = <p className={classes.error}>error.message</p>;
+  }
+  if (isLoading) {
+    content = "Loading Meals...";
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        <ul>{content}</ul>
       </Card>
     </section>
   );
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from "react";
+
+const useHttp = (applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(
+    async (requestConfig) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method || "GET",
+          headers: requestConfig.headers || {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+        const data = await response.json();
+        applyData(data);
+      } catch (error) {
+        setError(error.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    },
+    [applyData]
+  );
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
